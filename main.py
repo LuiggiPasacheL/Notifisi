c9d7d23fa13bf3c9bbbd63447f3ed0676d62153d
LuiggiPasacheL <luiggi.pasache.lopera@gmail.com>
2022-11-04 17:32:28 -0500

    feat: scheduling extraction of news

--- main.py
+++ main.py
@@ -41,15 +41,37 @@ except:
     storage.save_news()
 
 # begin of event to schedule
-count_new_news = storage.get_count_new_news(incoming_news)
+def find_news():
+    # TODO: Avoid duplicate code
+    html = get_html(domain, path)
+    soup = BeautifulSoup(html, 'html.parser')
 
-if count_new_news > 0:
-    storage.replace_news(count_new_news, incoming_news)
-    storage.save_news()
-    notify("We found news", f"{count_new_news} news founded")
+    titles = get_titles(soup)
+    descriptions = get_descriptions(soup)
+    links = get_links(soup, domain)
+
+    incoming_news = create_news_array(titles, descriptions, links)
+    # Duplicate
+
+    count_new_news = storage.get_count_new_news(incoming_news)
+
+    if count_new_news == 0:
+        storage.replace_news(count_new_news, incoming_news)
+        storage.save_news()
+        notify("We found news", f"{count_new_news} news founded")
 
 # end of event
 
-print("-----------------------------------------------------")
-print(f"Cantidad de noticias: {len(storage.news)}")
-print(f"Cantidad de noticias nuevas: {count_new_news}")
+import schedule
+import time
+
+schedule.every().day.at(config["time"]).do(find_news)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
+
+
+# print("-----------------------------------------------------")
+# print(f"Cantidad de noticias: {len(storage.news)}")
+# print(f"Cantidad de noticias nuevas: {count_new_news}")
