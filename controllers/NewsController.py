5a6fa7e2b7e9d64e1b10c4247e3a0f67bf838f14
LuiggiPasacheL <luiggi.pasache.lopera@gmail.com>
2022-11-04 18:02:56 -0500

    refactor: Extract logic from news to Controller

---
+++ controllers/NewsController.py
@@ -0,0 +1,63 @@
+
+from bs4 import BeautifulSoup
+from models.Notifier import notify
+from models.News import News
+import requests
+
+def get_titles(html_parsed):
+    titles_tag = html_parsed.find_all('h4', 'mfp_carousel_title')
+    titles = list(map(lambda title_tag: title_tag.a.string, titles_tag))
+    return titles
+
+def get_descriptions(html_parsed):
+    descriptions_tag = html_parsed.find_all('p', 'mfp_carousel_introtext')
+    descriptions = list(map(lambda descriptions_tag: descriptions_tag.string, descriptions_tag))
+    return descriptions
+
+def get_links(html_parsed, domain):
+    titles_tag = html_parsed.find_all('h4', 'mfp_carousel_title')
+    links = list(map(lambda title_tag: domain + title_tag.a['href'], titles_tag))
+    return links
+
+def create_news_array(titles, descriptions, links):
+    total_titles = len(titles)
+    total_descriptions = len(descriptions)
+    total_links = len(links)
+    
+    total = 0
+    if total_titles == total_descriptions == total_links:
+        total = total_titles
+
+    news_array = []
+    for news_index in range(total):
+        news_array.append(News(titles[news_index], descriptions[news_index], links[news_index]))
+
+    return news_array
+
+def get_html(domain, path):
+    url = domain + path
+
+    response = requests.get(url, verify=False)
+    html = response.text
+
+    return html
+
+def find_news(domain, path):
+    html = get_html(domain, path)
+    soup = BeautifulSoup(html, 'html.parser')
+
+    titles = get_titles(soup)
+    descriptions = get_descriptions(soup)
+    links = get_links(soup, domain)
+
+    incoming_news = create_news_array(titles, descriptions, links)
+
+    return incoming_news
+
+def compare_news(storage, incoming_news):
+    count_new_news = storage.get_count_new_news(incoming_news)
+
+    if count_new_news == 0:
+        storage.replace_news(count_new_news, incoming_news)
+        storage.save_news()
+        notify("We found news", f"{count_new_news} news founded")
--- main.py
+++ main.py
@@ -1,20 +1,10 @@
 
-from bs4 import BeautifulSoup
-
-import requests
 import json
+import schedule
+import time
 
-from models.News import get_links, get_descriptions, get_titles, create_news_array
 from models.NewsStorage import NewsStorage
-from models.Notifier import notify
-
-def get_html(domain, path):
-    url = domain + path
-
-    response = requests.get(url, verify=False)
-    html = response.text
-
-    return html
+from controllers.NewsController import find_news, compare_news
 
 with open("config.json", "r") as f:
     config = json.load(f)
@@ -23,54 +13,19 @@ with open("config.json", "r") as f:
 domain = config['page']['domain']
 path = config['page']['path']
 
-html = get_html(domain, path)
-soup = BeautifulSoup(html, 'html.parser')
-
-titles = get_titles(soup)
-descriptions = get_descriptions(soup)
-links = get_links(soup, domain)
-
-incoming_news = create_news_array(titles, descriptions, links)
-
 storage = NewsStorage()
 
 try:
     storage.load_news()
 except:
-    storage.news = incoming_news
+    storage.news = find_news()
     storage.save_news()
 
-# begin of event to schedule
-def find_news():
-    # TODO: Avoid duplicate code
-    html = get_html(domain, path)
-    soup = BeautifulSoup(html, 'html.parser')
-
-    titles = get_titles(soup)
-    descriptions = get_descriptions(soup)
-    links = get_links(soup, domain)
-
-    incoming_news = create_news_array(titles, descriptions, links)
-    # Duplicate
-
-    count_new_news = storage.get_count_new_news(incoming_news)
-
-    if count_new_news == 0:
-        storage.replace_news(count_new_news, incoming_news)
-        storage.save_news()
-        notify("We found news", f"{count_new_news} news founded")
-
-# end of event
-
-import schedule
-import time
-
-schedule.every().day.at(config["time"]).do(find_news)
+schedule.every().day.at(config["time"]).do(compare_news, storage=storage, incoming_news=find_news(domain, path)).tag("")
 
 while True:
     schedule.run_pending()
     time.sleep(1)
-
 
 # print("-----------------------------------------------------")
 # print(f"Cantidad de noticias: {len(storage.news)}")
