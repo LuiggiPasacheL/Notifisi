7b4f00b53ac7d1b9a6b762e28a22d8eccd8876b8
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2022-11-05 17:57:17 -0500

    Feat: Add new entry to menu systray

--- controllers/TrayController.py
+++ controllers/TrayController.py
@@ -5,19 +5,23 @@ from config import *
 from controllers.NewsController import find_news, renew_and_notify_news
 import os
 import sys
+import webbrowser
+
+try:
+    base_path = sys._MEIPASS
+except Exception:
+    base_path = os.path.abspath(".")
+image_path = os.path.join(base_path, 'assets', 'logo.png')
 
 def create_stray(storage):
     domain = config['page']['domain']
     path = config['page']['path']
-    try:
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-    image_path = os.path.join(base_path, 'assets', 'logo.png')
     image = Image.open(image_path)
 
     def on_click(icon, item):
-        if str(item) == "Recargar noticias":
+        if str(item) == "Abrir noticias":
+            webbrowser.open(domain + path + '#tns1-item1')
+        elif str(item) == "Recargar noticias":
             incoming_news = find_news(domain, path)
             renew_and_notify_news(storage, incoming_news)
         elif str(item) == "Salir":
@@ -26,6 +30,7 @@ def create_stray(storage):
             print("Feature not supported")
 
     icon = pystray.Icon("Notifisi", image, menu=pystray.Menu(
+        pystray.MenuItem("Abrir noticias", on_click),
         pystray.MenuItem("Recargar noticias", on_click),
         pystray.MenuItem("Salir", on_click)
     ))
