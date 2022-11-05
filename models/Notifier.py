480bc81342f80a92e93f23fe657a1ca19d2f6032
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2022-11-05 17:50:05 -0500

    Feat: add image to notification

--- models/Notifier.py
+++ models/Notifier.py
@@ -1,8 +1,20 @@
 
+import os
+import sys
 from notifypy import Notify
-notification = Notify()
+
+try:
+    base_path = sys._MEIPASS
+except Exception:
+    base_path = os.path.abspath(".")
+
+notification = Notify(
+    default_notification_application_name='Notifisi', 
+    default_notification_icon=os.path.join(base_path, 'assets', 'logo.png')
+)
 
 def notify(title, message):
     notification.title = title
     notification.message = message
+    notification.icon = os.path.join(base_path, 'assets', 'logo.png')
     notification.send()
