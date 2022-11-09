95eef60a1b85801142cdfcc5843a48b5bda11727
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2022-11-09 15:37:28 -0500

    refactor: clean main

--- main.py
+++ main.py
@@ -10,6 +10,16 @@ from controllers.NotifyController import notify
 from controllers.SystrayController import create_systray
 from controllers.NewsController import find_news, renew_and_notify_news
 
+def review_news(domain, path):
+    incoming_news = find_news(domain, path)
+    try:
+        schedule.every().day.at(config.time).do(renew_and_notify_news,storage=storage, incoming_news=incoming_news).tag("renew_news")
+    except schedule.ScheduleValueError:
+         schedule.every().day.do(renew_and_notify_news,storage=storage, incoming_news=incoming_news).tag("renew_news")
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 if __name__ == "__main__":
     storage = NewsStorage()
 
@@ -19,16 +29,6 @@ if __name__ == "__main__":
         storage.insert_news(find_news(config.domain, config.path))
         storage.save_news()
         notify("Se encontraron noticias", f"{len(storage.news)} noticias encontradas")
-
-    def review_news(domain, path):
-        incoming_news = find_news(domain, path)
-        try:
-            schedule.every().day.at(config.time).do(renew_and_notify_news,storage=storage, incoming_news=incoming_news).tag("renew_news")
-        except schedule.ScheduleValueError:
-             schedule.every().day.do(renew_and_notify_news,storage=storage, incoming_news=incoming_news).tag("renew_news")
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
 
     schedule_thread = threading.Thread(target=review_news, args=(config.domain, config.path,), daemon=True)
     schedule_thread.start()
